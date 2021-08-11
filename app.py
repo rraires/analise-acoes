48a2eba240339a176e97ff2250c1a633cd940ed9
rraires <rraires@gmail.com>
2021-08-11 16:28:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 import streamlit as st
 st.title('Acesse a nova versão do site AnaliseQuant')
+st.sidebar.image('./analisequant_logo-removebg.png')
 st.write("[www.analisequant.com](http://www.analisequant.com)")
