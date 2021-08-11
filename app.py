31f31e31fb77506ac0119ed085a55aadc7660d18
rraires <rraires@gmail.com>
2021-08-11 17:18:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import streamlit as st
+rimport streamlit as st
 col1, col2, col3 = st.beta_columns([1,1,1])
 with col2:
-  st.title('Acesse a nova versão do site AnaliseQuant')
+  st.header('Nova Versão')
   st.image('./analisequant_logo-removebg.png')
   st.write("[www.analisequant.com](http://www.analisequant.com)")
