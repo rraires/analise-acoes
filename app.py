ad512ce5439ccdca0c15c76779e7edf779f04811
rraires <rraires@gmail.com>
2021-08-11 16:37:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
-st.title('Acesse a nova versão do site AnaliseQuant')
-st.image('./analisequant_logo-removebg.png')
-st.write("[www.analisequant.com](http://www.analisequant.com)")
+col1, col2, col3 = st.beta_columns([1,1,1])
+  with col2:
+    st.title('Acesse a nova versão do site AnaliseQuant')
+    st.image('./analisequant_logo-removebg.png')
+    st.write("[www.analisequant.com](http://www.analisequant.com)")
